8b9ddad108841da50696b9a004804d568a03f958
StaryGoryla <134088884+StaryGoryla@users.noreply.github.com>
2023-09-13 19:44:27 +0200

    Add files via upload

---
+++ users/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+from django.contrib import admin
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+    path("login/", auth_views.LoginView.as_view(template_name="users/login.html"), name="login"),
+    path("logout/", views.logout, name="logout"),
+    path("register/", views.register, name="register"),
+    path("admin/", admin.site.urls),
+    path("login_gui/", views.login_gui, name="login_gui"),
+    path('register_gui/', views.register_gui, name="register_gui"),
+    path('logout_gui/', views.logout_gui, name="logout_gui"),
+    path('active/', views.active, name="active")
+]
---
+++ website/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.contrib.sessions.models import Session
+from django.contrib.auth.models import User
+from django.utils import timezone
+import json
+from django.http import JsonResponse
+from users.models import CustomUser
+
+
+def home(request):
+    online_users = CustomUser.objects.filter(is_online=True)
+    context = {'online_users': online_users}
+    return render(request, "home.html", context)
+
+
+@login_required()
+def game(request):
+    return render(request, "game.html")
+
+
+@login_required()
+def stats(request):
+    return render(request, "stats.html")
+
+
+@login_required()
+def profile(request):
+    return render(request, "profile.html")
+
